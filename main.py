476ed8e46a4069205d333111ba2cd92d0e3707ba
Luís Eduardo <luiseduardogfranca@gmail.com>
2017-10-31 14:10:56 -0300

    update code main.puy

--- main.py
+++ main.py
@@ -1,102 +1,73 @@
 #-*- coding utf-8 -*-
-
 import numpy as np
 import matplotlib.pyplot as plt
-from data_process import cleanData
 import pandas as pd
+from data_process import train_features, train_labels
 
-#defining the activation function
-def sig(x, deriv=False):
-	if deriv == True:
-		return sig(x)*(1-sig(x))
-
+def sigmoid(x, deriv=False):
+	if deriv:
+		return sigmoid(x)*(1-sigmoid(x))
+	#1/1+e^-x
 	return 1/(1+np.exp(-x))
 
-#aparting labels and features
-x = [i[:-1] for i in cleanData]
-y = [i[-1] for i in cleanData]
-
-bias = np.ones(len(x))
-
-x = np.array(x)
-y = np.array(y)
-
-temp = []
-
-for i in y:
-	temp.append([1,0]) if i == 2 else temp.append([0,1])
-
-y = np.array(temp)
-
-#Adding the bias term
-x = np.c_[x, bias]
-
-percent = 0.9
-
-x = np.array([i for i in x[:int(percent*len(x))]])
-x_test = np.array([i for i in x[int((1-percent)*len(x)):]])
-ft = pd.DataFrame(data=x_test)
-ft.to_csv('ft.csv',sep=',',encoding='utf-8',index=False)
-
-y = np.array([i for i in y[:int(percent*len(y))]])
-y_test = np.array([i for i in y[int((1-percent)*len(y)):]])
-lt = pd.DataFrame(data=y_test)
-lt.to_csv('lt.csv',sep=',',encoding='utf-8',index=False)
-
-np.random.seed(1)
-n = 0.5
-
 #Number of neurons on input layer
-l0_len = x.shape[-1]
+l0_len = train_features.shape[-1]
 #Neurons on hidden layer
 l1_len = 50
 #Neurons on output layer
 l2_len = 2
 
-eta = 0.5
+#learning rate
+eta = 0.55
 
+np.random.seed(1)
 #defining weigths random
 #l0_len x l1_len
-w1 = 2*np.random.random((l0_len ,l1_len))-1
+# w1 = 2*np.random.random((l0_len, l1_len))-1
+w1 = np.random.uniform(-1,1,(l0_len, l1_len))
 
 #l1_len x l2_len
-w2 = 2*np.random.random((l1_len,l2_len))-1
+# w2 = 2*np.random.random((l1_len, l2_len))-1
+w2 = np.random.uniform(-1,1,(l1_len, l2_len))
 
+# print(np.random.random((l0_len, l1_len)))
 errors = []
 
-for i in range(3000):
-	l0 = x
-	#calculating the hide layer output
-	l1 = sig(l0.dot(w1))
-	#calculating the output layer result
-	l2 = sig(l1.dot(w2))
+def train():
+	global w1, w2, l0, errors
+	for i in range(20000):
+		l0 = train_features
 
-	#abosulte error
-	l2_error = y - l2
+		l1 = sigmoid(l0.dot(w1))
 
-	mean_error = np.mean(np.abs(l2_error))
-	
-	if i%100 == 0:
-		errors.append(mean_error)
+		l2 = sigmoid(l1.dot(w2))
 
-	if mean_error < 0.1:
-		break
+		error = train_labels-l2
+		# error = ((train_labels - l2)**2)*0.5
 
-	#ote
-	l2_error = sig(l2, deriv=True)*l2_error
+		mean_error = np.mean(np.abs(error))
+		# print(mean_error)
 
-	l1_error = sig(l1, deriv=True)*(np.dot(l2_error,w2.T))
+		if i%100 == 0:
+			# print(mean_error)
+			errors.append(mean_error)
 
-	l1_delta = eta*(l0.T.dot(l1_error))
-	l2_delta = eta*(l1.T.dot(l2_error))
+		l2_error = sigmoid(l2, deriv=True) * error
 
-	w1 += l1_delta
-	w2 += l2_delta
+		l1_error = sigmoid(l1, deriv=True) * (np.dot(l2_error, w2.T))
 
-df1 = pd.DataFrame(data=w1)
-df2 = pd.DataFrame(data=w2)
+		l1_delta = eta*(l0.T.dot(l1_error))
+		l2_delta = eta*(l1.T.dot(l2_error))
 
-df1.to_csv('w1.csv',sep=',',encoding='utf-8',index=False)
-df2.to_csv('w2.csv',sep=',',encoding='utf-8',index=False)
-plt.plot(list(range(len(errors))), errors)
-plt.show()
+		w1 += l1_delta
+		w2 += l2_delta
+
+#
+# w1 = pd.DataFrame(data = w1)
+# w2 = pd.DataFrame(data = w2)
+#
+# w1.to_csv('new-w1.csv',sep=',',encoding='utf-8',index=False)
+# w1.to_csv('new-w2.csv',sep=',',encoding='utf-8',index=False)
+
+# plt.plot(list(range(len(errors))), errors)
+# plt.show()
